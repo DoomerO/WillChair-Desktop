b99eb4d5b4714a9cc4d94cf3d9ae7e1cc08d8931
João Victor Almeida Rodrigues <joaovictoralmeida0710@gmail.com>
2023-11-15 21:30:07 -0300

    TESTE2 MANO

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,24 +2,27 @@ const urls = ['index.html', 'offline.html'];
 
 self.addEventListener('install', (event) => {
     console.log("SW Installed")
-    event.waitUntil(
+    /*event.waitUntil(
         caches.open("Cache").then((cache) => {
             console.log("Cache oppened");
             cache.addAll(urls);
         })
-    );
+    );*/
 });
 
 self.addEventListener('fetch', (e) => {
+    console.log(e);
+    /*
     e.respondWith(
         caches.match(e.request).then((res) => {
             return fetch(e.request).catch(() => caches.match('offline.html'));
         })
-    );
+    );*/
 });
 
 self.addEventListener('activate', (e) => {
-    const cacheWhiteList = [];
+    console.log("SW activated")
+    /*const cacheWhiteList = [];
     cacheWhiteList.push("Cache");
     e.waitUntil(caches.keys().then((cacheNames) => Promise.all(
         cacheNames.map((cashN) => {
@@ -27,6 +30,6 @@ self.addEventListener('activate', (e) => {
                 return caches.delete(cashN);
             }
         })
-    )))
+    )))*/
 })
 
